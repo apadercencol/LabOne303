c9397f0714578c5575f6043345689f5b9ee555b1
apadercencol <144699114+apadercencol@users.noreply.github.com>
2024-10-16 05:33:55 -0400

    Add files via upload

---
+++ Mid/AllenMidterm/src/main/java/com/spring/jpa/AllenMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllenMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllenMidtermApplication.class, args);
+	}
+
+}
